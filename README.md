5e0840f21cc4d4f0e2f98305b7d2f8eb34697fb3
matt <43501106+snowcot@users.noreply.github.com>
2021-01-09 14:35:49 +0800

    finished intended purpose

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-<img src="https://static.wikia.nocookie.net/yokaiwatch/images/7/7f/Yoink_Art.jpg" width="100px"></img>
-
 # tweetyoinker
+
+<img src="https://static.wikia.nocookie.net/yokaiwatch/images/7/7f/Yoink_Art.jpg" width="150px" align="right"></img>
 
 A mini program written in Python to scrape media off of a tweet, originally planned to scrape an entire twitter profile, but due to limitations from Twitter API, this functionality is not implemented
 
@@ -8,7 +8,16 @@ A mini program written in Python to scrape media off of a tweet, originally plan
 
 Enter a link, the program will identify the type of media, if successful, it will display a direct download link.
 
-[try on repl.it](https://www.youtube.com/watch?v=M5V_IXMewl4)
+[try on repl.it](https://repl.it/@mattlab/tweetyoinker)
 
+it works on images!
 
-(screenshots coming soon)
+![image demo](https://a.pomf.cat/bexons.png)
+
+videos
+
+![video demo](https://a.pomf.cat/rcfuly.png)
+
+and last but not least, albums!
+
+![album demo](https://a.pomf.cat/skibuw.png)
--- yoink.py
+++ yoink.py
@@ -1,51 +1,75 @@
 import requests, json
 from requests_oauthlib import OAuth1Session
 
-# authentication (v1.1 and v2 respectively)
-oauthv1 = OAuth1Session('<api token>', client_secret='<api secret>', resource_owner_key='<resource owner key>', resource_owner_secret='<resource owner secret>')
-oauthv2 = {"Authorization": "Bearer <token>"}
+# authentication (v1.1 // oauth 1.0)
+oauthv1 = OAuth1Session('<api token>', client_secret='<api secret>', resource_owner_key='<account token>', resource_owner_secret='<account secret>')
 
-def getUserID(username):
-    UIDrequest = "https://api.twitter.com/2/users/by?usernames={}".format(username)
-    UIDresponse = requests.get(UIDrequest, headers=oauthv2)
-
-    # why this mess? 
-    # twitter api response nests an object inside of an array (which is inside of an object)
-    # i had to learn json syntax to finally be able to do this (＃`Д´)	
-    # worth it, i think 
-    return UIDresponse.json()['data'][0]['id']
-
-def getFleet(userID):
-    # fleet: a group of ships sailing together, engaged in the same activity, or under the same ownership.
-    # so, essentially, a list of tweets in this context
-    fleetRequest = "https://api.twitter.com/2/users/{}/tweets?expansions=attachments.media_keys,attachments.poll_ids,geo.place_id&tweet.fields=conversation_id,created_at".format(userID)
-    fleetResponse = requests.get(fleetRequest, headers=oauthv2) 
-
-    return fleetResponse
-
-
-# what happened here?
-# it looks like twitter api v2 doesn't really look good for media fetching, rolling back to v1 just for this purpose
+# twitter api v2 doesn't really look good for media fetching, using v1.1
 def getTweet(tweetID):
-    tweetRequest = "https://api.twitter.com/1.1/statuses/show.json?id={}".format(tweetID)
+    tweetRequest = "https://api.twitter.com/1.1/statuses/show.json?id={}&tweet_mode=extended".format(tweetID)
     tweetResponse = oauthv1.get(tweetRequest)
 
     return tweetResponse
 
-def fleetOwner(username):
-    # line 16-17 for explaination
-    userID = getUserID(username)
-    tweetResponse = getFleet(userID)
-    debug_jprint(tweetResponse)
+def processing(tweetResponse):
+    tweetResponse = tweetResponse.json()
 
-def tweetQuery(tweetID):
-    tweetResponse = getTweet(tweetID)
-    debug_jprint(tweetResponse)
+    # this will attempt to catch tweets without media, program will exit when it isn't
+    try:
+        mediatype = tweetResponse['extended_entities']['media'][0]['type']
+    except KeyError:
+        print("No media detected")
+        return
 
+    print("\n\n========== yoink results ==========")
+    print("Creation: " + tweetResponse['created_at'])
+
+    # twitter api will truncate certain tweets based on length, so, if the text field is not detected, there will be certain a full_text field
+    try:
+        print("Tweet Contents: " + tweetResponse['text'])
+    except:
+        print("Tweet Contents: " + tweetResponse['full_text'])
+
+    print("User: " + tweetResponse['user']['name'] + " (@" + tweetResponse['user']['screen_name'] + ")")
+    print("Media Type: " + mediatype)
+    
+    if (mediatype == 'photo'):
+        print("Media Direct Link(s);")
+        
+        for photo in tweetResponse['extended_entities']['media']:
+            print("• " + photo['media_url_https'])
+            print("Source Media Resolution: " + str(photo['sizes']['large']['h']) + " height - " + str(photo['sizes']['large']['w']) + " width")
+    elif (mediatype == 'video'):
+        print("Duration: " + str(tweetResponse['extended_entities']['media'][0]['video_info']['duration_millis'] / 1000) + " second(s)")
+        
+        print("Media Direct Link(s);")
+        for variant in tweetResponse['extended_entities']['media'][0]['video_info']['variants']:
+            if (variant['content_type'] == "application/x-mpegURL"):
+                break
+            print("• Bitrate [" + str(variant['bitrate']) + "] --- URL: " + variant['url'])
+        
+        print("Source Media Resolution: " + str(tweetResponse['extended_entities']['media'][0]['sizes']['large']['h']) + " height - " + str(tweetResponse['extended_entities']['media'][0]['sizes']['large']['w']) + " width")    
+
+    return
+
+def inputFormatting(userInput):
+    userInput = userInput.split("/")
+
+    if (len(userInput) == 1):
+        tweetID = userInput[0]
+    else:
+        if (userInput[2] == "twitter.com"):
+            tweetID = userInput[5]
+        elif (userInput[0] == "twitter.com"):
+            tweetID = userInput[3]
+
+    return tweetID
+
+# debug method to show what is in the response
 def debug_jprint(obj):
-    # create a formatted string of the Python JSON object
     text = json.dumps(obj.json(), sort_keys=True, indent=4)
     print(text)
 
-def debug_printResponse(response):
-    debug_jprint(response.json())
+userInput = input("tweetyoinker!\n==================\nEnter your twitter link here (or tweet ID)\n\n")
+processing(getTweet(inputFormatting(userInput)))
+
